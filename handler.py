25bd0e2d6b50ae88f05b54f81dea4439c2896821
Aryan <aryan4p130202@gmail.com>
2023-12-28 18:52:14 +0530

    refactored code and separated validators

--- handler.py
+++ handler.py
@@ -6,22 +6,31 @@ import re
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('UsersTable')
 
+def validate_mobile_number(mob_num):
+    return bool(re.match(r'^\d{10}$', mob_num))
+
+def validate_pan_number(pan_num):
+    return bool(re.match(r'^[A-Z]{5}[0-9]{4}[A-Z]$', pan_num))
+
+def validate_input(full_name, mob_num, pan_num):
+    if not full_name.strip():
+        return False, "Full name cannot be empty"
+    if not validate_mobile_number(mob_num):
+        return False, "Invalid mobile number format"
+    if not validate_pan_number(pan_num):
+        return False, "Invalid PAN number format"
+    return True, None
+
 def create_user(event, context):
     try:
         data = json.loads(event['body'])
-        full_name = data['full_name']
-        mob_num = data['mob_num']
-        pan_num = data['pan_num']
+        full_name = data.get('full_name')
+        mob_num = data.get('mob_num')
+        pan_num = data.get('pan_num')
 
-        # Validation
-        if not full_name:
-            return {"statusCode": 400, "body": json.dumps({ "error": "Full name cannot be empty" })}
-        
-        if not re.match(r'^\d{10}$', mob_num):
-            return {"statusCode": 400, "body": json.dumps({ "error": "Invalid mobile number format" })}
-        
-        if not re.match(r'^[A-Z]{5}[0-9]{4}[A-Z]$', pan_num):
-            return {"statusCode": 400, "body": json.dumps({ "error": "Invalid PAN number format" })}
+        is_valid, validation_error = validate_input(full_name, mob_num, pan_num)
+        if not is_valid:
+            return {"statusCode": 400, "body": json.dumps({"error": validation_error})}
 
         user_id = str(uuid.uuid4())
 
@@ -33,23 +42,16 @@ def create_user(event, context):
             'pan_num': pan_num
         })
 
-        return {"statusCode": 200, "body": json.dumps({ "message": "User created successfully" })}
+        return {"statusCode": 200, "body": json.dumps({"message": "User created successfully"})}
 
     except Exception as e:
         return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
-        
+
 def get_users(event, context):
     try:
         # Retrieve all user records from the DynamoDB table
         response = table.scan()
         items = response.get('Items', [])
-
-        # If no users are found, return an empty list
-        if not items:
-            return {
-                "statusCode": 200,
-                "body": json.dumps({"users": []})
-            }
 
         # Format user records according to specified structure
         users = [{
@@ -62,9 +64,7 @@ def get_users(event, context):
         # Return a JSON object containing the user records
         return {
             "statusCode": 200,
-            "body": json.dumps({
-                "users": users
-            })
+            "body": json.dumps({"users": users})
         }
 
     except Exception as e:
@@ -76,26 +76,17 @@ def get_users(event, context):
 
 def get_single_user(event, context):
     try:
-        # Extract user_id from the query parameters or request body
-        user_id = event.get('queryStringParameters', {}).get('user_id')  # Modify based on how you pass user_id
+        user_id = event.get('queryStringParameters', {}).get('user_id')
 
         if not user_id:
-            return {
-                "statusCode": 400,
-                "body": json.dumps({"error": "User ID is required"})
-            }
+            return {"statusCode": 400, "body": json.dumps({"error": "User ID is required"})}
 
-        # Retrieve user record from DynamoDB using user_id
         response = table.get_item(Key={'user_id': user_id})
         user_data = response.get('Item')
 
         if not user_data:
-            return {
-                "statusCode": 404,
-                "body": json.dumps({"error": "User not found with the provided user_id"})
-            }
+            return {"statusCode": 404, "body": json.dumps({"error": "User not found with the provided user_id"})}
 
-        # Format user details according to the specified structure
         user_details = {
             "user_id": user_data['user_id'],
             "full_name": user_data['full_name'],
@@ -103,86 +94,60 @@ def get_single_user(event, context):
             "pan_num": user_data['pan_num']
         }
 
-        # Return the JSON object containing user details
-        return {
-            "statusCode": 200,
-            "body": json.dumps(user_details)
-        }
+        return {"statusCode": 200, "body": json.dumps(user_details)}
 
     except Exception as e:
-        # Handle any exceptions and return an error response
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }        
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
 
 def delete_user(event, context):
     try:
         data = json.loads(event['body'])
         user_id = data.get('user_id')
 
-        # Check if user_id exists in the database
         response = table.get_item(Key={'user_id': user_id})
         if 'Item' not in response:
-            return {
-                "statusCode": 404,
-                "body": json.dumps({"error": "User not found!"})
-            }
+            return {"statusCode": 404, "body": json.dumps({"error": "User not found!"})}
 
-        # Delete user record from the database
         table.delete_item(Key={'user_id': user_id})
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "User deleted successfully"})
-        }
+        return {"statusCode": 200, "body": json.dumps({"message": "User deleted successfully"})}
 
     except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
 
 def update_user(event, context):
     try:
         data = json.loads(event['body'])
         user_id = data.get('user_id')
-        update_data = data.get('update_data')
+        update_data = data.get('update_data', {})
 
-        # Check if user_id exists in the database
         response = table.get_item(Key={'user_id': user_id})
         if 'Item' not in response:
-            return {
-                "statusCode": 404,
-                "body": json.dumps({"error": "User not found with the provided user_id"})
-            }
+            return {"statusCode": 404, "body": json.dumps({"error": "User not found with the provided user_id"})}
 
-        # Extract and validate update_data
         full_name = update_data.get('full_name')
         mob_num = update_data.get('mob_num')
         pan_num = update_data.get('pan_num')
 
-        # Validation for updated data
-        if full_name and not full_name.strip():
+        if full_name is not None and not full_name.strip():
             return {"statusCode": 400, "body": json.dumps({"error": "Full name cannot be empty"})}
 
-        if mob_num and not re.match(r'^\d{10}$', mob_num):
+        if mob_num is not None and not validate_mobile_number(mob_num):
             return {"statusCode": 400, "body": json.dumps({"error": "Invalid mobile number format"})}
 
-        if pan_num and not re.match(r'^[A-Z]{5}[0-9]{4}[A-Z]$', pan_num):
+        if pan_num is not None and not validate_pan_number(pan_num):
             return {"statusCode": 400, "body": json.dumps({"error": "Invalid PAN number format"})}
 
-        # Update user data in the database
         update_expression = 'SET '
         expression_attribute_values = {}
 
-        if full_name:
+        if full_name is not None:
             update_expression += 'full_name = :fn, '
             expression_attribute_values[':fn'] = full_name
-        if mob_num:
+        if mob_num is not None:
             update_expression += 'mob_num = :mn, '
             expression_attribute_values[':mn'] = mob_num
-        if pan_num:
+        if pan_num is not None:
             update_expression += 'pan_num = :pn, '
             expression_attribute_values[':pn'] = pan_num
 
@@ -194,13 +159,8 @@ def update_user(event, context):
             ExpressionAttributeValues=expression_attribute_values
         )
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "User updated successfully"})
-        }
+        return {"statusCode": 200, "body": json.dumps({"message": "User updated successfully"})}
 
     except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
+
